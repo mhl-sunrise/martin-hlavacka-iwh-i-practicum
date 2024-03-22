2ca218f8670c3e4e63e238864063c70b7a78c5ee
mhl-sunrise <mhl@sunrise.dk>
2024-03-22 13:12:09 +0100

    Rewriting removal

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 /**
- * Note: 
+ * Note: My test account did not have access to Custom Objects, so I followed your documentation and used Company object instead.
+ * Link: https://app-eu1.hubspot.com/academy/144354397/tracks/1092124/1093824/5493?language=en
  */
 
+/** Load ENV variables */
 require('dotenv').config();
 
+/** Import node modules */
 const express = require('express');
 const axios = require('axios');
 const app = express();
 
+/** App settings*/
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+/** Load Private App Access Token from .env */
 const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
-/** My test account did not have access to Custom Objects, so I followed your documentation and used Company object instead. */
+/** Show existing companies */
 app.get('/', async (req, res) => {
     const companies = 'https://api.hubspot.com/crm/v3/objects/companies?properties=name,cvr_number,pot_number';
     const headers = {
@@ -31,6 +36,7 @@ app.get('/', async (req, res) => {
     }
 });
 
+/** Create new company */
 app.post('/create-cobj', async (req, res) => {
     const update = {
         properties: {
@@ -52,47 +58,41 @@ app.post('/create-cobj', async (req, res) => {
     }
 });
 
+/** Delete existing company */
+app.delete('/delete-cobj', async (req, res) => {
 
-// * Code for Route 1 goes here
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+    // const companies = 'https://api.hubspot.com/crm/v3/objects/companies/' + req.body.id;
+    // const headers = {
+    //     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    //     'Content-Type': 'application/json'
+    // }
+    // try {
+    //     const resp = await axios.delete(companies, { headers });
+    //     res.redirect('/');
+    // } catch (error) {
+    //     console.error(error);
+    // }
+});
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+/** Open existing company */
+app.get('/open-cobj', async (req, res) => {
+    const company = 'https://api.hubspot.com/crm/v3/objects/companies/' + req.query.id + "?properties=name,pot_number,cvr_number"
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
     }
-
+    try {
+        const resp = await axios.get(company, { headers });
+        const data = resp.data;
+        console.log(data);
+        res.render('edit', { title: 'Companies | Martin Hlavacka', data});      
+    } catch (error) {
+        console.error(error);
+    }
 });
-*/
 
+/** Update existing company */
 
-// * Localhost
+/** Start the app */
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
